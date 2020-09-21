dd7f5129605d3316fd5be28bbb2f421b3e06c52b
Gerardo-Guizar <phdgcloudp@gmail.com>
2020-09-20 21:17:29 -0500

    Updates

---
+++ precache-manifest.6370b733441439b92bdef91a44e540b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "861ea25406ff2bf2ce4fda4734de982a",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "c9a187fc063825609026",
+    "url": "/portafolio/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "f2dd895eabebce5bf66d",
+    "url": "/portafolio/static/js/2.d890bdf9.chunk.js"
+  },
+  {
+    "revision": "c9a187fc063825609026",
+    "url": "/portafolio/static/js/main.a0d37e11.chunk.js"
+  },
+  {
+    "revision": "9fd29bcc1d4ba73bed23",
+    "url": "/portafolio/static/js/runtime~main.41300385.js"
+  },
+  {
+    "revision": "f986d7e78dae0a3be2030613bafc111f",
+    "url": "/portafolio/static/media/profile.f986d7e7.jpg"
+  }
+]);
